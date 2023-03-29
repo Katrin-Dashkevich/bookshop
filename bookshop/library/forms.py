cbdab81bfdaefa32f70f7deb5eb1c58e1c2843c5
Katrin Dashkevich <75749226+Katrin-Dashkevich@users.noreply.github.com>
2023-03-29 12:05:26 +0300

    Update forms.py

--- bookshop/library/forms.py
+++ bookshop/library/forms.py
@@ -1,22 +1,22 @@
-from .models import Book, Author, Library
-from django.forms import ModelForm, TextInput, DateField
+# from .models import Book, Author, Library
+# from django.forms import ModelForm, TextInput, DateField
 
 
-class AddForm(ModelForm):
-    class Meta:
-        model = Book, Author, Library
-        fields =["title", "authors", "publication_date"]
-        widgets = {
-            "title": TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Название книги'
-            }),
-            "authors": TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Автор/Авторы'
-            }),
-            "publication_date": DateField(attrs={
-                'class': 'form-control',
-                'placeholder': 'Дата добавления'
-            }),
-        }
+# class AddForm(ModelForm):
+#     class Meta:
+#         model = Book, Author, Library
+#         fields =["title", "authors", "publication_date"]
+#         widgets = {
+#             "title": TextInput(attrs={
+#                 'class': 'form-control',
+#                 'placeholder': 'Название книги'
+#             }),
+#             "authors": TextInput(attrs={
+#                 'class': 'form-control',
+#                 'placeholder': 'Автор/Авторы'
+#             }),
+#             "publication_date": DateField(attrs={
+#                 'class': 'form-control',
+#                 'placeholder': 'Дата добавления'
+#             }),
+#         }
